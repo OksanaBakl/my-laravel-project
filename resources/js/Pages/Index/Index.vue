78f92d71be5e16ecc1c87486da6a63067f684c90
OksanaBakl <oksana.molesha@gmail.com>
2025-03-06 22:19:41 +0100

    Initial commit

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,19 @@
+
+<template>
+<!--        <div>Hello  {{ // counter }}!</div>-->
+        <p>THe message is: {{ message }}</p>
+        <Link href="/hello">Show page</Link>
+</template>
+
+<script setup>a
+// import {ref} from 'vue';
+import {Link} from '@inertiajs/vue3';
+
+defineProps({
+    message: String
+})
+
+// const counter = ref(0)
+// setInterval(() => counter.value++, 1000)
+</script>
+
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,9 @@
+<template>
+        <div>Show component</div>
+        <Link href="/">Index page</Link>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
+
